6871f1e4cc6ca220721eec3b2c6366a0859f898e
gabox2002 <gabox2002@gmail.com>
2023-03-23 16:25:19 -0300

    m

--- js/index.js
+++ js/index.js
@@ -4,9 +4,4 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");/*Si esta la clase la elimina si no esta la añade */
 
-    if (navMenu.classList.contains("nav-menu_visible")){
-        navToggle.setAttribute("aria-label", "Cerrar menú");
-    } else {
-        navToggle.setAttribute("aria-label", "Abrir menú");
-    }
 }); 
