ac9105a1e99a4f322ab62844e45e3eb44ed18bc0
gabox2002 <gabox2002@gmail.com>
2023-04-02 16:12:58 -0300

    m

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");/*Si esta la clase la elimina si no esta la añade */
-
-}); 
